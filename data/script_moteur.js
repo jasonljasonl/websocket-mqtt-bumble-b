c5591572826fce8fcaf22c92c7f0312f4daa2ea5
jasonljasonl <jasonlandim@outlook.fr>
2024-08-23 20:08:10 +0200

    ajout de commentaires

--- data/script_moteur.js
+++ data/script_moteur.js
@@ -1,50 +1,48 @@
+// Variable de la vitesse (définit à 95 par défaut)
 let currentSpeed = 95;
+// Variable de la vitesse MAX (vitesse MAX des moteurs, équivalent à 165tr/min)
 const maxSpeed = 4095;
+// Variable de la vitesse MIN (moteurs pas assez puissant pour démarrer la voiture, équivaut à 0)
 const minSpeed = 95;
+// Valeur qui incrémentera la vitesse max à chaque appuis de boutons
+// 95 / 1095 / 2095 / 3095 / 4095 
 const step = 1000;
-const maxSpeedMS = 0.56; // Vitesse max en m/s
+// Vitesse max en m/s
+const maxSpeedMS = 0.56;
 
+// Calcule la vitesse actuelle de la voiture grâce à la position du joystick, de la valeurs max des moteurs et la vitesse max en M/S
 function calculateSpeedMS(joystickValue) {
   return (joystickValue / maxSpeed) * maxSpeedMS;
 }
 
+// Récupère l'élément qui affiche la vitesse gràace a son ID et la met a jour à chaque fois que la vitesse change
 function updateSpeedDisplay(joystickValue) {
   let speedMS = calculateSpeedMS(joystickValue);
   document.getElementById("vitesseVoiture").innerText = `${speedMS.toFixed(2)} m/s`;
 }
 
-// Envoi de la vitesse à Node-RED
-function sendSpeedToNodeRed(speed) {
-  fetch('http://localhost:1880/speed', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ speed: speed })
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch((error) => console.error('Error:', error));
-}
-
+// Augmente la vitesse maximale du régulateur de vitesse
 function increaseSpeedLevel() {
+  // Si la vitesse actuelle + la valeur d'incrémentation est inférieur à la vitesse max
   if (currentSpeed + step <= maxSpeed) {
+    // augmente la vitesse max du régulateur de vitesse
     currentSpeed += step;
+    // Met a jour l'élément qui affiche la vitesse en affichant la nouvelle vitesse max du régulateur
     updateSpeedDisplay(currentSpeed);
-
-    // Envoyer la vitesse mise à jour à Node-RED
-    sendSpeedToNodeRed(calculateSpeedMS(currentSpeed));
   }
+  // Affiche dans la console l'action effectuée
   console.log('Vitesse augmentée à :', currentSpeed);
 }
 
+// Réduit la vitesse maximale de la voiture
 function decreaseSpeedLevel() {
+  // Si la vitesse actuelle - la valeur d'incrémentation est supérieur à la vitesse min
   if (currentSpeed - step >= minSpeed) {
+    // réduit la vitesse max du régulateur de vitesse
     currentSpeed -= step;
+    // Met a jour l'élément qui affiche la vitesse en affichant la nouvelle vitesse max du régulateur
     updateSpeedDisplay(currentSpeed);
-
-    // Envoyer la vitesse mise à jour à Node-RED
-    sendSpeedToNodeRed(calculateSpeedMS(currentSpeed));
   }
+  // Affiche dans la console l'action effectuée
   console.log('Vitesse réduite à :', currentSpeed);
 }
--- data/script_websocket.js
+++ data/script_websocket.js
@@ -1,6 +1,8 @@
+// Variable du lien du serveur websocket
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 
+// Initialisation du serveur websocket
 function initWebSocket() {
   console.log('Trying to open a WebSocket connection...');
   websocket = new WebSocket(gateway);
@@ -9,18 +11,28 @@ function initWebSocket() {
 }
 
 function onOpen(event) {
+  // Indique à l'utilisateur quand le websocket est ouvert
   console.log('Connection opened');
 }
 
+
 function onClose(event) {
+  // Indique à l'utilisateur quand le websocket est fermé
   console.log('Connection closed');
+  // et retente la connexion après 2000 millisecondes
   setTimeout(initWebSocket, 2000);
 }
 
+
+// Fonction qui enverra les commandes vers le websocket (communication avec la voiture)
 function sendCommand(cmd, data) {
   const command = {
+    // cmd = clé du json
     cmd: cmd,
+    // data = valeur de la clé
     data: data
+    // Exemple : cmd : "1" / data : [2000,2000,2000,2000] = "clé pointant les moteurs" , [valeur de la vitesse des moteurs]
   };
+  // Envoi les commandes sous forme de chaîne de caractère
   websocket.send(JSON.stringify(command));
 }
