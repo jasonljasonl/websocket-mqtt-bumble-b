1f48d7d12378806ca69c80f6efbeca238dc1817e
jasonljasonl <jasonlandim@outlook.fr>
2024-08-23 20:07:29 +0200

    ajout commentaires

--- data/script_main.js
+++ data/script_main.js
@@ -1,9 +1,15 @@
 window.addEventListener('load', onLoad);
 
+// Initialise les fonctions au chargement du script
 function onLoad() {
+  // Initialise la fonction du websocket
   initWebSocket();
+  // Initialise la fonction du joystick des roues
   initJoystick();
+  // Initialise la fonction du joystick de la caméra
   initJoystickCam();
-  updateSpeedDisplay(0); // Initial display
-  updateJoystickOrientation(0, 0); // Initial orientation display
+  // Initialise la vitesse affichée à 0 
+  updateSpeedDisplay(0); 
+  // Initialise la position du joystick à 0
+  updateJoystickOrientation(0, 0); 
 }
