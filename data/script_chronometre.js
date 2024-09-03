8d151ef5a33a782cbe01f2fa548128d00da85faf
jasonljasonl <jasonlandim@outlook.fr>
2024-09-03 11:31:18 +0200

    ajout envoi vers BDD

--- data/script_chronometre.js
+++ data/script_chronometre.js
@@ -35,6 +35,7 @@ function stopRace() {
   document.getElementById("stopRace").disabled = true;
 
   calculateAverageSpeed();
+  sendToMysql(raceData);
 }
 
 function updateRaceTimer() {
@@ -48,8 +49,46 @@ function updateRaceTimer() {
   function calculateAverageSpeed() {
     const averageSpeed = totalSpeed / speedCount;
     console.log(`Vitesse moyenne : ${averageSpeed.toFixed(2)} m/s`);
+  }
   
-    // Envoyer la vitesse moyenne à Node-RED ou à une base de données
-    sendAverageSpeedToNodeRed(averageSpeed);
+
+
+  const raceData = {
+    id: 2,
+    averageSpeed: totalSpeed,
+    distanceCovered: 46,
+    timeSpent: raceDuration,
+    wheelRotationSpeed: 71
+  };
+
+
+  function sendToMysql(raceData) {
+    // URL du backend
+    const url = 'http://localhost:8080/races';
+  
+    // Token d'authentification (à remplacer par votre token)
+    const token = 'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0ZXN0MSIsImlhdCI6MTcyNTM1NTE0MCwiZXhwIjoxNzI1NDQxNTQwfQ.tAhLVI2z1_cH45vey4LVzBxQDmt-UevkyxVgC2MdA3DMefI57LsYKA30MvV1iwp8';
+  
+    // Configuration de la requête fetch
+    fetch(url, {
+      method: 'POST', // Méthode d'envoi
+      headers: {
+        'Authorization': `Bearer ${token}`, // Ajout du token JWT dans l'en-tête Authorization
+        'Content-Type': 'application/json' // Le contenu envoyé est du JSON
+      },
+      body: JSON.stringify(raceData) // Corps de la requête contenant les données de la course
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Transformation de la réponse en JSON
+    })
+    .then(json => {
+      console.log('Données envoyées avec succès :', json); // Affichage de la réponse du serveur
+    })
+    .catch(error => {
+      console.error('Erreur lors de l\'envoi des données :', error); // Gestion des erreurs
+    });
   }
   
